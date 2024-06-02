fc46f220d2048290d0a83565d429dafdc800bca4
Stephen <stephen.swaringin@gmail.com>
2024-06-01 21:29:53 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+module.exports = class ShoppingCart {
+  constructor(items = []) {
+    this.__items = items;
+  }
+  get subtotal() {
+    return this.__items.reduce((acc, item) => {
+      return acc + item.quantity * item.price;
+    }, 0);
+  }
+};
